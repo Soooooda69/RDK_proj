1ee17e2034d684f18772f870949231daccc34908
Soooooda69 <2285124906@qq.com>
2023-04-22 16:10:16 -0400

    update

--- Lab3/UR5control_test.m
+++ Lab3/UR5control_test.m
@@ -3,12 +3,12 @@ clc
 clear
 ur5=ur5_interface();
 ur5.move_joints(ur5.home, 3);
-pause(5);
+pause(4);
 
 q1=[pi/8; pi/8; pi/8; pi/8; pi/8; pi/8];
-ur5.move_joints(q1+ur5.home, 5);
-pause(6);
-q2 = [pi/8; pi/7; pi/7; pi/8; pi/6; pi/6];
+ur5.move_joints(q1+ur5.home, 3);
+pause(4);
+q2 = [pi/6; pi/7; pi/7; pi/8; pi/6; pi/8];
 gdesired=ur5FwdKin(q2);
-ur5RRcontrol(gdesired, 1, ur5);
+ur5RRcontrol(gdesired, 0.1, ur5);
 pause(20);
